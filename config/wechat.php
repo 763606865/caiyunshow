7389d3a38988c64945af0dfcc30308660e8c0131
liujunlin <763606865@qq.com>
2023-06-15 18:29:25 +0800

    enable

--- config/wechat.php
+++ config/wechat.php
@@ -6,6 +6,7 @@ return [
             'app_id' => env('WECHAT_BUSINESS_APP_ID'),
             'app_secret' => env('WECHAT_BUSINESS_APP_SECRET'),
             'crypt' => [
+                'enable' => env('WECHAT_BUSINESS_CRYPT_ENABLE'),
                 'sn' => env('WECHAT_BUSINESS_CRYPT_SN'),
                 'key' => env('WECHAT_BUSINESS_CRYPT_KEY'),
             ],
